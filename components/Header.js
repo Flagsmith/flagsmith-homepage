97c4d9f4fc6661ef2549dddf6270130287cf914d
kyle <kyle@solidstategroup.com>
2020-07-12 17:59:30 +0100

    re-add project roadmap

--- components/Header.js
+++ components/Header.js
@@ -73,6 +73,9 @@ class Header extends PureComponent {
                                 <Link prefetch={false} href="/open-source"><a className="nav-link">Open Source</a></Link>
                             </li>
                             <li className="nav-item">
+                                <a className="nav-link" rel="noopener noreferrer" target="_blank" href="https://product-hub.io/roadmap/5d81f2406180537538d99f28">Product Roadmap</a>
+                            </li>
+                            <li className="nav-item">
                                 <Link prefetch={false} href={`${Project.appUrl}/login`}><a className="nav-link">Log in</a></Link>
                             </li>
                             <li className="nav-item active">
