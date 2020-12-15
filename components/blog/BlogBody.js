6351dfdb8f8c71fcd1ecfbddb9e7113f82e9ad84
Ben Rometsch <ben@solidstategroup.com>
2020-12-15 09:51:52 +0000

    Added podcast episode links

--- components/blog/BlogBody.js
+++ components/blog/BlogBody.js
@@ -19,7 +19,9 @@ const BlogBody = ({ sections }) => (
             case ('text'):
                 return <Text data={slice} key={`slice-${index}`} />;
             case ('about_author'):
-                return (<div><h2>Links from the Episode</h2><AboutAuthor data={slice} key={`slice-${index}`} /></div>);
+                return (<AboutAuthor data={slice} key={`slice-${index}`} />);
+            case ('episode_links'):
+                return (<div><h2>Links from the Episode</h2><Text data={slice} key={`slice-${index}`} /></div>);
             case ('markdown'):
                 return <Markdown data={slice} key={`slice-${index}`} />;
             default: {
